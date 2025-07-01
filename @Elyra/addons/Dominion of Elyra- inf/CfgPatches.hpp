c2eebf71b153735377c7074a61c8f00f1101dd81
nicole-the-terrible <nicolepongchuenprapada@gmail.com>
2025-06-30 21:18:53 -0700

    Elyra faction: infantry added

---
+++ @Elyra/addons/Dominion of Elyra- inf/CfgPatches.hpp
@@ -0,0 +1,57 @@
+class CfgPatches {
+    class O_ElyraArmy {
+        units[] = {
+            "B_OElyraArmy_2nd_LT_01",
+            "B_OElyraArmy_1st_LT_01",
+            "B_OElyraArmy_SSGT_01",
+            "B_OElyraArmy_SGTMAJ_01",
+            "B_OElyraArmy_SGT_01",
+            "B_OElyraArmy_CPL_01",
+            "B_OElyraArmy_PVT_01",
+            "B_OElyraArmy_CPT_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "A3_Characters_F_Exp",
+            "A3_Aegis_Characters_F_Aegis",
+            "A3_Characters_F",
+            "CLE_BloodIron",
+            "EAW_Chinese_Infantry",
+            "EAW_Japanese_Infantry",
+            "EAW_SNLF_Infantry",
+            "EAW_Civ",
+            "EAW_Femoid",
+            "EAW_KneeMortar",
+            "lambs_danger",
+            "WW2_SPE_Assets_c_Characters_Germans_c_GER_Wehrmacht",
+            "WW2_SPE_Assets_c_Characters_Germans_c_GER_Sturmtroopers",
+            "kka3_gestures_ace",
+            "A3_Sounds_F",
+            "A3_Sounds_F_Exp",
+            "lambs_range",
+            "NORTH_pervitin",
+            "sounds_f_vietnam_c",
+            "ace_gforces",
+            "ace_interaction",
+            "ace_movement",
+            "ace_medical_feedback",
+            "A3_Data_F",
+            "A3_Data_F_Curator",
+            "A3_Air_F_Heli",
+            "ace_thermals",
+            "lambs_formations",
+            "zen_attributes",
+            "ace_refuel",
+            "WW2_SPE_Assets_c_Vehicles_Extended_HUD_c",
+            "boat_f_vietnam_c",
+            "wheeled_f_vietnam_c",
+            "boat_f_vietnam_03_c",
+            "CUP_Core",
+            "armor_f_vietnam_c",
+            "cba_xeh"
+        };
+        author = "Nicole the terrible";
+        authors[] = {"Nicole the terrible"};
+    };
+};
